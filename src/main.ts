1c1cee58dca4b1e026ab8d86ffca39ae62a421fe
fan <291950304@qq.com>
2024-09-25 16:22:02 +0800

    chore: 引入 shadcn-vue 样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from 'pinia'
 import './assets/iconfont/iconfont.css'
 import './assets/iconfont-avatar/iconfont-avatar.css'
 import './style.css'
+import './assets/index.css'
+
 import App from './App.vue'
 
 const pinia = createPinia()
