9b6c24ead3dac4abdd03eb34c0a73744cbc3b1b0
fan <291950304@qq.com>
2024-10-17 17:28:51 +0800

    chore: align hook

---
+++ src/hooks/align.ts
@@ -0,0 +1,32 @@
+export function align() {
+  const iconList = [
+    {
+      id: 1,
+      icon: 'icon-shejiqi-2zuoduiqi',
+    },
+    {
+      id: 2,
+      icon: 'icon-shejiqi-3juzhongduiqi',
+    },
+    {
+      id: 3,
+      icon: 'icon-shejiqi-1youduiqi',
+    },
+    {
+      id: 4,
+      icon: 'icon-shejiqi-5shangduiqi',
+    },
+    {
+      id: 5,
+      icon: 'icon-shejiqi-6shuipingduiqi',
+    },
+    {
+      id: 6,
+      icon: 'icon-shejiqi-4dibuduiqi',
+    },
+  ]
+
+  return {
+    iconList,
+  }
+}
