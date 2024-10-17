3fd31f66becd86fec68d53c3870a60cd2e7e4c55
fan <291950304@qq.com>
2024-10-17 15:46:58 +0800

    chore: text module

--- src/components/text-add.vue
+++ src/components/text-add.vue
@@ -1,9 +1,87 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue'
+import { v4 as uuidV4 } from 'uuid'
+import * as fabric from 'fabric'
+import BigNumber from 'bignumber.js'
+import CollapsibleTool from './collapsible-tool.vue'
 import { Textarea } from '@/components/ui/textarea'
+import { Button } from '@/components/ui/button'
+import { useCanvasInfo } from '@/hooks/canvas-info'
+import { useLayers } from '@/hooks/layers'
+
+const text = ref<string>('')
+const textChange = computed({
+  get() {
+    return text.value
+  },
+  set(value) {
+    text.value = value
+  },
+})
+
+const canvasInfo = useCanvasInfo()
+const layers = useLayers()
+function createText() {
+  if (!textChange.value || textChange.value.trim() === '') {
+    return
+  }
+
+  const uuid = uuidV4()
+
+  if (canvasInfo.ctx) {
+    const textObj = new fabric.Textbox(textChange.value, {
+      uuid,
+      originX: 'center',
+      originY: 'center',
+      opacity: 1,
+      strokeWidth: 0,
+    })
+
+    const canvasWidth = canvasInfo.canvasWidth
+    const left = new BigNumber(canvasWidth).dividedBy(2).toNumber()
+    const top = left
+
+    textObj.set('left', left)
+    textObj.set('top', top)
+    const width = textObj.get('width').toFixed(0)
+    const height = textObj.get('height').toFixed(0)
+    const scaleX = textObj.get('scaleX')
+    const scaleY = textObj.get('scaleY')
+
+    canvasInfo.ctx.add(textObj)
+
+    layers.createPerfectText({
+      uuid,
+      text: textChange.value,
+      width,
+      height,
+      left,
+      top,
+      scaleX,
+      scaleY,
+      angle: 0,
+      flipX: false,
+      flipY: false,
+      opacity: 1,
+    })
+  }
+}
 </script>
 
 <template>
-  <div>
-    <Textarea />
+  <div v-bind="$attrs">
+    <CollapsibleTool title="创建文本">
+      <div class="p-2">
+        <Textarea v-model="textChange" />
+        <div class="flex gap-2 mt-2">
+          <Button class="flex-1" @click="createText">
+            创建
+          </Button>
+          <Button class="flex-1" variant="outline" @click="text = ''">
+            清空
+          </Button>
+        </div>
+      </div>
+    </CollapsibleTool>
   </div>
 </template>
