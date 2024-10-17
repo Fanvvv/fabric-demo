02cdc04a898274845d8fc2668a504d2e19f42483
fan <291950304@qq.com>
2024-10-17 13:57:51 +0800

    chore: use collapsible-tool component

--- src/components/color-background-selector.vue
+++ src/components/color-background-selector.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useCanvasInfo } from '../hooks/canvas-info'
 import ColorSelectorItem from './color-selector-item.vue'
+import CollapsibleTool from './collapsible-tool.vue'
 
 const colors = ['#985', '#124', '#952', '#156', '#802', '#092']
 
@@ -20,24 +21,23 @@ function handleSetCurrentColor(color: string | null) {
 </script>
 
 <template>
-  <div>
-    <div>
-      背景颜色选择
-    </div>
-    <div class="flex flex-wrap gap-2 mt-2">
-      <ColorSelectorItem
-        :close="true"
-        :active="canvasInfoStore.ctx?.backgroundColor === 'transparent'"
-        @click="handleSetCurrentColor(null)"
-      />
-
-      <div v-for="item in colors" :key="item">
+  <div class="w-[350px]">
+    <CollapsibleTool title="背景颜色选择">
+      <div class="flex flex-wrap gap-2 mt-2">
         <ColorSelectorItem
-          :color="item"
-          :active="canvasInfoStore.ctx?.backgroundColor === item"
-          @click="handleSetCurrentColor(item)"
+          :close="true"
+          :active="canvasInfoStore.ctx?.backgroundColor === 'transparent'"
+          @click="handleSetCurrentColor(null)"
         />
+
+        <div v-for="item in colors" :key="item">
+          <ColorSelectorItem
+            :color="item"
+            :active="canvasInfoStore.ctx?.backgroundColor === item"
+            @click="handleSetCurrentColor(item)"
+          />
+        </div>
       </div>
-    </div>
+    </CollapsibleTool>
   </div>
 </template>
