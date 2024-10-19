fd0157e728abc874a5a7f1009bdfd7e076532e2e
fan <291950304@qq.com>
2024-10-19 14:37:57 +0800

    chore: 添加文本图层的操作

--- src/components/layers-item.vue
+++ src/components/layers-item.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { v4 as uuidV4 } from 'uuid'
-import type * as fabric from 'fabric'
 import type { ILayerItemAtImage, ILayerItemAtText } from '@/hooks/layers'
-import { ImageTypes, useLayers } from '@/hooks/layers'
-import { useCanvasInfo } from '@/hooks/canvas-info'
+import { ImageTypes } from '@/hooks/layers'
+import { useLayerOptions } from '@/hooks/layer-options'
+import { useClickThumbnail } from '@/hooks/click-thumbnail'
+import { useCurrentSelected } from '@/hooks/current-selected'
+import TextOptions from '@/components/text-options.vue'
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from '@/components/ui/collapsible'
 
 interface IProps {
   itemObj: ILayerItemAtImage | ILayerItemAtText
@@ -13,88 +15,67 @@ const props = withDefaults(defineProps<IProps>(), {
   itemObj: () => ({} as ILayerItemAtImage),
 })
 
-const canvasInfo = useCanvasInfo()
-const layers = useLayers()
-function handleDelete(uuid: string) {
-  const ctx = canvasInfo.ctx
-  if (uuid && ctx) {
-    const obj = ctx.getObjects().find(obj => obj.get('uuid') === uuid)
-    if (obj) {
-      ctx.remove(obj)
+const { handleCopy, handleDelete } = useLayerOptions()
 
-      // 同步 pinia 数据
-      layers.deleteLayerItem(uuid)
-    }
-  }
-}
-
-async function handleCopy(uuid: string, type: ImageTypes) {
-  const ctx = canvasInfo.ctx
-  if (uuid && ctx) {
-    const objects = ctx.getObjects()
-    const objIndex = objects.findIndex(obj => obj.get('uuid') === uuid)
-    const object = objects[objIndex]
-    if (object) {
-      const newUUID = uuidV4()
-      const layerItemInfo = layers.getLayerItemByUUID(uuid)
-      if (type === ImageTypes.TEXT && object.get('type') === 'textbox' && layerItemInfo?.type === ImageTypes.TEXT) {
-        const newText = await object.clone() as fabric.Text
-        newText.set('uuid', newUUID)
-        ctx.insertAt(objIndex, newText)
-
-        layers.cloneText({
-          ...layerItemInfo,
-          uuidOld: uuid,
-          uuid: newUUID,
-        })
-      }
-    }
-  }
-}
+const currentSelected = useCurrentSelected()
+const { isOpen, handleClickThumbnail } = useClickThumbnail(props.itemObj.uuid)
 </script>
 
 <template>
   <div class="my-2 p-2 border border-gray-200 rounded-md">
-    <div class="flex items-center gap-2">
-      <div class="w-16 h-16 flex-shrink-0 rounded-md overflow-hidden border border-gray-200 cursor-pointer">
-        <template v-if="props.itemObj.type === ImageTypes.IMAGE">
-          1
-        </template>
-        <template v-if="props.itemObj.type === ImageTypes.TEXT">
-          <div class="flex items-center justify-center w-full h-full">
-            <i class="iconfont icon-tianjiawenzi text-2xl text-dark-900" />
-          </div>
-        </template>
-      </div>
-      <div class="flex flex-wrap gap-2 flex-1">
-        <div>
-          <span class="text-sm">
-            宽度&nbsp;
-          </span>
-          <span class="text-gray-500">
-            {{ props.itemObj.width }} px
-          </span>
-        </div>
-        <div>
-          <span class="text-sm">
-            高度&nbsp;
-          </span>
-          <span class="text-gray-500">
-            {{ props.itemObj.height }} px
-          </span>
-        </div>
-      </div>
+    <Collapsible
+      v-model:open="isOpen"
+    >
       <div class="flex items-center gap-2">
-        <i
-          class="iconfont icon-shejiqi-fuzhi cursor-pointer"
-          @click="handleCopy(props.itemObj.uuid, props.itemObj.type)"
-        />
-        <i
-          class="iconfont icon-shejiqi-shanchu cursor-pointer"
-          @click="handleDelete(props.itemObj.uuid)"
-        />
-        <i class="iconfont icon-tuodong handle-icon cursor-pointer" />
+        <CollapsibleTrigger as-child>
+          <div
+            class="w-16 h-16 flex-shrink-0 rounded-md overflow-hidden border  cursor-pointer"
+            :class="currentSelected.uuid === props.itemObj.uuid ? 'border-red-900' : 'border-gray-200'"
+            @click="handleClickThumbnail(props.itemObj.uuid, currentSelected.uuid === props.itemObj.uuid)"
+          >
+            <template v-if="props.itemObj.type === ImageTypes.IMAGE">
+              1
+            </template>
+            <template v-if="props.itemObj.type === ImageTypes.TEXT">
+              <div class="flex items-center justify-center w-full h-full">
+                <i class="iconfont icon-tianjiawenzi text-2xl text-dark-900" />
+              </div>
+            </template>
+          </div>
+        </CollapsibleTrigger>
+        <div class="flex flex-wrap gap-2 flex-1">
+          <div class="text-xs">
+            <span>
+              宽度&nbsp;
+            </span>
+            <span class="text-gray-500">
+              {{ props.itemObj.width }} px
+            </span>
+          </div>
+          <div class="text-xs">
+            <span>
+              高度&nbsp;
+            </span>
+            <span class="text-gray-500">
+              {{ props.itemObj.height }} px
+            </span>
+          </div>
+        </div>
+        <div class="flex items-center gap-2">
+          <i
+            class="iconfont icon-shejiqi-fuzhi cursor-pointer"
+            @click="handleCopy(props.itemObj.uuid, props.itemObj.type)"
+          />
+          <i
+            class="iconfont icon-shejiqi-shanchu cursor-pointer"
+            @click="handleDelete(props.itemObj.uuid)"
+          />
+          <i class="iconfont icon-tuodong handle-icon cursor-pointer" />
+        </div>
       </div>
-    </div>
+      <CollapsibleContent class="space-y-2">
+        <TextOptions />
+      </CollapsibleContent>
+    </Collapsible>
   </div>
 </template>
