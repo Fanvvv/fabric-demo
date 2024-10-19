7efdf050d15928531bb5707405622d66560fe37a
fan <291950304@qq.com>
2024-10-19 17:28:00 +0800

    chore: file to base64

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// 将文件转换为 base64 格式
+export function file2Base64(file: File): Promise<string> {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      return resolve(String(e.target?.result))
+    }
+    reader.readAsDataURL(file)
+  })
+}
