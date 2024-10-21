b6e1eb673a836f2c5df4b7a7faeb3776e2878c30
fan <291950304@qq.com>
2024-10-21 14:58:13 +0800

    chore: 键盘按键快捷功能

---
+++ src/hooks/keyboard.ts
@@ -0,0 +1,65 @@
+import { useCurrentSelected } from './current-selected'
+import { useLayerOptions } from './layer-options'
+import { useCanvasInfo } from './canvas-info'
+import { useLayers } from './layers'
+
+export function useKeyboard() {
+  const { handleDelete } = useLayerOptions()
+  const currentSelected = useCurrentSelected()
+  const canvasInfo = useCanvasInfo()
+  const layers = useLayers()
+
+  function handleMove(key: string) {
+    const activeObject = canvasInfo.ctx?.getActiveObject()
+    const uuid = activeObject?.get('uuid')
+    let top = activeObject?.get('top')
+    let left = activeObject?.get('left')
+
+    switch (key) {
+      case 'ArrowUp':
+        top -= 1
+        activeObject?.set('top', top)
+        break
+      case 'ArrowDown':
+        top += 1
+        activeObject?.set('top', top)
+        break
+      case 'ArrowLeft':
+        left -= 1
+        activeObject?.set('left', left)
+        break
+      case 'ArrowRight':
+        left += 1
+        activeObject?.set('left', left)
+        break
+    }
+
+    layers.updateLayerItem({
+      uuid,
+      left,
+      top,
+    })
+    activeObject?.setCoords()
+    canvasInfo.ctx?.renderAll()
+  }
+
+  function handleKeyDown(e: KeyboardEvent) {
+    if (e.key === 'Delete' || e.key === 'Backspace') {
+      if (currentSelected.uuid)
+        handleDelete(currentSelected.uuid)
+    }
+    if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
+      handleMove(e.key)
+    }
+  }
+
+  document.addEventListener('keydown', handleKeyDown)
+
+  function removeEvent() {
+    document.removeEventListener('keydown', handleKeyDown)
+  }
+
+  return {
+    removeEvent,
+  }
+}
