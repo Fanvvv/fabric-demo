0c1bf76e71359e87a6627e3c3756c8e9daa867e3
fan <291950304@qq.com>
2024-10-21 14:58:50 +0800

    chore: 对齐方式按钮组件

---
+++ src/components/align-btn.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useAlign } from '@/hooks/align'
+import { Button } from '@/components/ui/button'
+
+const { iconList, handleAlignItem } = useAlign()
+</script>
+
+<template>
+  <div class="border-t border-gray-200 py-2 mt-2">
+    <div class="text-sm font-bold mb-2">
+      对齐方式
+    </div>
+    <div class="flex gap-2">
+      <template v-for="item in iconList" :key="item.id">
+        <Button variant="outline" size="icon" :title="item.name" @click="handleAlignItem(item.id)">
+          <i class="iconfont" :class="item.icon" />
+        </Button>
+      </template>
+    </div>
+  </div>
+</template>
