87e913f7d86881117fda619e1c97ae19fd44cf7c
fan <291950304@qq.com>
2024-10-21 17:04:07 +0800

    chore: 添加 formatFileSize 方法

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -15,3 +15,17 @@ export function file2Base64(file: File): Promise<string> {
     reader.readAsDataURL(file)
   })
 }
+
+// 文件大小添加单位
+export function formatFileSize(sizeInBytes: number) {
+  const units = ['B', 'KB', 'MB', 'GB', 'TB']
+  let size = sizeInBytes
+  let unitIndex = 0
+
+  while (size >= 1024 && unitIndex < units.length - 1) {
+    size /= 1024
+    unitIndex++
+  }
+
+  return `${size.toFixed(2)} ${units[unitIndex]}`
+}
