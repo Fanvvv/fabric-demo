008628b7243784a6a757c16b51a185de13feb464
fan <291950304@qq.com>
2024-10-22 16:55:57 +0800

    chore: 添加 loading 效果

--- src/components/ui/button/Button.vue
+++ src/components/ui/button/Button.vue
@@ -8,6 +8,7 @@ interface Props extends PrimitiveProps {
   variant?: ButtonVariants['variant']
   size?: ButtonVariants['size']
   class?: HTMLAttributes['class']
+  loading?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -20,7 +21,25 @@ const props = withDefaults(defineProps<Props>(), {
     :as="as"
     :as-child="asChild"
     :class="cn(buttonVariants({ variant, size }), props.class)"
+    :disabled="loading"
   >
-    <slot />
+    <div class="flex items-center gap-1">
+      <svg
+        v-if="loading"
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        class="animate-spin"
+      >
+        <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+      </svg>
+      <slot />
+    </div>
   </Primitive>
 </template>
