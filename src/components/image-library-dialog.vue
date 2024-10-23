f6d95cba3a0320f536fcf48e8bfef3ca465163d4
fan <291950304@qq.com>
2024-10-23 09:14:48 +0800

    chore: 图库弹窗

--- src/components/image-library-dialog.vue
+++ src/components/image-library-dialog.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { ref } from 'vue'
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
-  DialogClose,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { getAllFiles } from '@/lib/indexedDB'
-import { file2Base64, formatFileSize } from '@/lib/utils'
-import { Checkbox } from '@/components/ui/checkbox'
+import { deleteFilesByUUID, getAllFiles } from '@/lib/indexedDB'
+import { cn, file2Base64, formatFileSize } from '@/lib/utils'
+import { showAlert, showConfirmDialog } from '@/lib/alert'
+import { useAddImage } from '@/hooks/add-image'
+import UploadFile from '@/components/upload-file.vue'
+import { Input } from '@/components/ui/input'
+
+interface IFileItem {
+  uuid: string
+  name: string
+  url: string
+  size: string
+  selected: boolean
+}
 
 // 从 indexedDB 中获取所有图片
-const filesList = ref()
-onMounted(async () => {
+const filesList = ref<IFileItem[]>([])
+const selectedFiles = ref<IFileItem[]>([])
+
+async function getFilesList() {
   const files = await getAllFiles()
 
   filesList.value = await Promise.all(files.map(async file => ({
@@ -24,21 +36,96 @@ onMounted(async () => {
     name: file.name,
     url: await file2Base64(file.data),
     size: formatFileSize(file.data.size),
+    selected: false, // 是否选中
   })))
-})
+}
 
 const isOpen = ref(false)
 
-function handleConfirm() {
-  console.log('confirm')
-  isOpen.value = false
+async function handleOpen() {
+  isOpen.value = true
+  await getFilesList()
+}
+
+function handleSelect(file: IFileItem) {
+  file.selected = !file.selected
+  if (file.selected) {
+    selectedFiles.value.push(file)
+  }
+  else {
+    selectedFiles.value = selectedFiles.value.filter(f => f.uuid !== file.uuid)
+  }
+}
+
+const { fileUploadedCallback, onFileInputChange } = useAddImage()
+const loading = ref(false)
+async function handleConfirm() {
+  try {
+    loading.value = true
+    if (selectedFiles.value.length === 0) {
+      showAlert({
+        title: '请选择图片',
+        description: '请选择至少一张图片',
+        type: 'error',
+      })
+      return
+    }
+    await Promise.all(selectedFiles.value.map(async (file) => {
+      await fileUploadedCallback(file.url, file.uuid, file.name)
+    }))
+    selectedFiles.value = []
+    isOpen.value = false
+  }
+  catch (e) {
+    return e
+  }
+  finally {
+    loading.value = false
+  }
+}
+
+async function handleUpload() {
+  // 不需要上传到indexedDB
+  // await fileUploadedCallback(file, uuid)
+  await getFilesList()
+}
+
+function handleDelete() {
+  if (selectedFiles.value.length === 0) {
+    showAlert({
+      title: '请选择图片',
+      description: '请选择至少一张图片',
+      type: 'error',
+    })
+    return
+  }
+  showConfirmDialog({
+    title: '删除图片',
+    description: '确定要删除选中的图片吗？',
+  }).then(async (res) => {
+    if (res) {
+      await deleteFilesByUUID(selectedFiles.value.map(file => file.uuid))
+      selectedFiles.value = []
+      await getFilesList()
+      showAlert({
+        title: '删除成功',
+        type: 'success',
+      })
+    }
+  })
+}
+
+const fileInput = ref<typeof Input | null>(null)
+
+function handleAddImage() {
+  fileInput.value?.$el.click()
 }
 </script>
 
 <template>
-  <Dialog :open="isOpen">
+  <Dialog v-model:open="isOpen">
     <DialogTrigger as-child>
-      <Button class="w-full mt-2" @click="isOpen = true">
+      <Button class="w-full mt-2" @click="handleOpen">
         从图库中选择
       </Button>
     </DialogTrigger>
@@ -48,29 +135,55 @@ function handleConfirm() {
       </DialogHeader>
 
       <div
-        class="w-full h-[500px] overflow-y-auto flex flex-wrap gap-2"
+        v-if="filesList.length"
+        class="w-full flex flex-col h-[500px] gap-2"
       >
-        <div
-          v-for="(file, index) in filesList"
-          :key="index"
-          class="relative w-[200px] flex flex-col overflow-hidden rounded-md shadow-sm hover:shadow-md
-          transition-all duration-300 cursor-pointer"
-        >
-          <Checkbox class="absolute top-2 right-2" />
-          <img class="w-[200px] h-[200px] object-cover" :src="file.url" alt="">
-          <div class="p-2">
-            <div class="text-sm whitespace-nowrap overflow-hidden text-ellipsis">
-              {{ file.name }}
-            </div>
-            <div class="text-xs text-gray-500">
-              {{ file.size }}
+        <div class="flex justify-end">
+          <Button class="mr-2" variant="outline" size="sm" @click="handleAddImage">
+            <span>上传</span>
+            <Input
+              ref="fileInput"
+              type="file"
+              :multiple="false"
+              class="hidden"
+              @change="onFileInputChange($event, getFilesList)"
+            />
+          </Button>
+          <Button variant="destructive" size="sm" @click="handleDelete">
+            删除
+          </Button>
+        </div>
+        <div class="w-full flex-1 overflow-y-auto flex flex-wrap gap-2">
+          <div
+            v-for="(file, index) in filesList"
+            :key="index"
+            :class="cn('relative w-[200px] max-h-[252px] flex flex-col overflow-hidden rounded-md border-2 border-transparent shadow-md hover:shadow-xl',
+                       'transition-shadow duration-300 cursor-pointer box-border', `${file.selected ? 'border-2 border-blue-500' : ''}`)"
+            @click="handleSelect(file)"
+          >
+            <img class="w-full h-[200px] object-cover" :src="file.url" alt="">
+            <div class="p-2">
+              <div class="text-sm whitespace-nowrap overflow-hidden text-ellipsis">
+                {{ file.name }}
+              </div>
+              <div class="text-xs text-gray-500">
+                {{ file.size }}
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <div v-else>
+        <div class="w-full flex flex-col items-center justify-center gap-2">
+          <div class="text-center text-gray-500">
+            暂无图片，请上传图片
+          </div>
+          <UploadFile class="w-full" @file-uploaded="handleUpload" />
+        </div>
+      </div>
       <DialogFooter as-child>
         <div class="flex gap-2">
-          <Button @click="handleConfirm">
+          <Button :loading="loading" @click="handleConfirm">
             确认
           </Button>
           <Button variant="outline" @click="isOpen = false">
